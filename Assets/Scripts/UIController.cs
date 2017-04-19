5f32381cc066705c679c7d588843b3096f970388
Isaac Bowes <isaac.bowes@hotmail.com>
2017-04-20 09:27:12 +0930

    no message

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject gameOverPanel;
+    public GameObject pauseMenu;
+
+    public PlayerHealth playerHealth;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.OnDeath += GameOverActive;
+            }
+            gameOverPanel.SetActive(false);
+        }
+        else
+        {
+            //Do Nothing
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (playerHealth != null)
+        {
+            playerHealth.OnDeath -= GameOverActive;
+        }
+
+    }
+
+    void GameOverActive()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_PS4
+        //Do Nothing
+#endif
+
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Greybox");
+    }
+}
