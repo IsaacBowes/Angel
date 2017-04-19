531f99d5ecd977f2f6679f625fb9d6b01b889d16
HarrisonAIE <harisunnyt@gmail.com>
2017-04-19 15:26:31 +0930

    no message

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 upAxis = new Vector3(0,0,-1);
+		Vector3 mouseScreenPosition = Input.mousePosition;
+		mouseScreenPosition.z = transform.position.z;
+		Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+		transform.LookAt(mouseWorldSpace, upAxis);
+		transform.eulerAngles = new Vector3(0,0,-transform.eulerAngles.z);
+	}
+}
