aab4a94a3c982b89a92663d0746a682027233701
Isaac Bowes <isaac.bowes@hotmail.com>
2017-04-19 16:46:25 +0930

    - Added a generic(base) health script. - Also added a PlayerHealth script inherited from the base.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public abstract class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [Tooltip("Maximum Health")]
+    public int maxHealth;
+    [Tooltip("Current Health")]
+    public int health;
+    [Tooltip("Is agent dead or not")]
+    public bool isDead;
+
+
+    public delegate void HealthEvent();
+    public event HealthEvent OnDeath;
+    public event HealthEvent OnHealthChange;
+
+    public abstract void Death();
+
+
+    //What happens when an agent gets damaged
+    public void AffectHealth(int healthDelta)
+    {
+        health += healthDelta;
+
+        if (OnHealthChange != null) { OnHealthChange(); } 
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        if (health <= 0 && isDead == false)
+        {
+            isDead = true;
+            if (OnDeath != null) { OnDeath(); } 
+            Death();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerHealth : Health
+{
+    public override void Death()
+    {
+        throw new NotImplementedException();
+    }
+}
