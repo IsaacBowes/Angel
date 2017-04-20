73453677a4065fa09da5f14672eb8e9b91b53552
Shannon Cross <shannonjamescross@yahoo.com.au>
2017-04-20 15:14:39 +0930

    no message

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour {
 
 	public GameObject player;
-<<<<<<< HEAD
 	public Waves waves;
 	public float SpawnRate;
-=======
->>>>>>> aad5a6e0e040f1c58bd7e2652c532b62573c2572
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +20,6 @@ public class SpawnEnemies : MonoBehaviour {
 
 	void SpawnEnemy()
 	{
-<<<<<<< HEAD
 		SpawnTier1 ();
 		SpawnTier2 ();
 		SpawnTier3 ();
@@ -32,14 +28,10 @@ public class SpawnEnemies : MonoBehaviour {
 	void SpawnTier3()
 	{
 		float RandomX = UnityEngine.Random.Range (0, 2);
-=======
-		float RandomX = Random.Range (0, 2);
->>>>>>> aad5a6e0e040f1c58bd7e2652c532b62573c2572
 		if (RandomX == 0)
 			RandomX = -17;
 		else
 			RandomX = 17;
-<<<<<<< HEAD
 		if (waves.waves [waves.WaveNumber].Tier3EnemyAmount > 0) {
 			AddEnemy ("Tier3Enemy", RandomX);
 			waves.waves [waves.WaveNumber].Tier3EnemyAmount -= 1;
@@ -75,9 +67,6 @@ public class SpawnEnemies : MonoBehaviour {
 	void AddEnemy(string str, float RandomX)
 	{
 		GameObject obj = Instantiate (Resources.Load (str), new Vector3 (RandomX, 0, UnityEngine.Random.Range (-18, 18)), Quaternion.Euler (0, 0, 0)) as GameObject;
-=======
-		GameObject obj = Instantiate (Resources.Load ("Tier3Enemy"), new Vector3 (RandomX, 0, Random.Range(-18, 18)), Quaternion.Euler(0, 0, 0)) as GameObject;
->>>>>>> aad5a6e0e040f1c58bd7e2652c532b62573c2572
 		obj.GetComponent<Enemy> ().player = player;
 	}
 }
